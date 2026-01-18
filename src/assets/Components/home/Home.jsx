64c3614f5443b42a2a01a88621f9fb5c959efd2e
deekshagarg <deekshagarg1405@gmail.com>
2026-01-18 17:29:13 +0530

    home1

--- src/assets/Components/home/Home.jsx
+++ src/assets/Components/home/Home.jsx
@@ -5,7 +5,75 @@ function Home(){
     return(
         <>
         
-<p>hello</p>
+<div className="home-main">
+<div className="hero-home">
+
+    
+<div className="hero-section">
+  <center>  <h1>College Event Management</h1></center>
+    <p>Streamline your campus events with our comprehensive platform. From registration to certificates, manage everything in one place.</p>
+
+    <div className="home-btn">
+        <button className="browser">Browser Events</button>
+        <button className="h-sign">Sign In</button>
+    </div>
+
+<div className="hero-card-main">
+
+   <div className="hero-card">
+    <div className="h-c-icon"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi-bi-calendar4" viewBox="0 0 16 16">
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+</svg></div>
+
+        <div className="h-c-p">
+            <h2>500+</h2>
+            <p>Events Hosted</p>
+        </div>
+    </div>
+
+
+   <div className="hero-card">
+    <div className="h-c-icon"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi-bi-people" viewBox="0 0 16 16">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+</svg></div>
+
+        <div className="h-c-p">
+            <h2>10K+</h2>
+            <p>Active Students</p>
+        </div>
+    </div>
+
+
+   <div className="hero-card">
+    <div className="h-c-icon"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
+  <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
+  <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
+</svg></div>
+
+        <div className="h-c-p">
+            <h2>8K+</h2>
+            <p>Certificates Issued</p>
+        </div>
+    </div>
+
+
+
+{/* ---hero-card------- */}
+</div>
+ 
+{/* ----hero-section--- */}
+</div>
+
+{/* ----hero-home--- */}
+</div>
+
+
+
+
+
+
+{/* ---------home-main------------- */}
+</div>
 
         </>
     )
